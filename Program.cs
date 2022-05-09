cbd7f816059d6a5ec602d891a4b758ebbe64f559
Satyam Wathrey <100951452+satyamwathrey@users.noreply.github.com>
2022-05-09 09:56:50 +0530

    Add files via upload
    workshop demo program

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace WorkshopDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "jupiter";
+            string str2 = "rejupit";
+
+            char [] ch1 =str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            String sort1 = new string(ch1);
+            String sort2 = new string(ch2);
+
+            if (sort1 == sort2)
+            {
+                Console.WriteLine("Both strings are anagrams");
+            }
+            else
+            {
+                Console.WriteLine("Both strings are not anagrams");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
